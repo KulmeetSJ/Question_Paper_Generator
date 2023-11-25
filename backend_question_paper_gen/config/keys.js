f1699154d33a575b8008d48b6bed481ca27c5e69
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-11-25 21:51:58 +0530

    Update keys.js

--- backend_question_paper_gen/config/keys.js
+++ backend_question_paper_gen/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://<username>:<password>@cluster0.pgcgkes.mongodb.net/?retryWrites=true&w=majority",
-    secretOrKey: "<your_secret_key"
+    mongoURI: "mongodb+srv://singhkulmeet3:japan238130@cluster0.pgcgkes.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "nQkP30sR6tUkA5vI7yGpN3sZ6wB8gA1v"
 };
