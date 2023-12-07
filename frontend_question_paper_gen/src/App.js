b78ea04553a822d3eca80e87d388a32fc2a43e65
Kulmeetcodez <singhkulmeet3@gmail.com>
2023-12-07 16:53:39 +0530

    Updating frontend

--- frontend_question_paper_gen/src/App.js
+++ frontend_question_paper_gen/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Help from './components/Help';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GeneratePaper from './components/GeneratePaper';
+import Navbar from './components/Navbar';
+import PreviousPapers from './components/PreviousPapers';
+import { Provider } from "react-redux";
+import store from "./store";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<><Navbar /><Home /></>} />
+          <Route path="/generate" element={<><Navbar /><GeneratePaper /></>} />
+          <Route path="/previous_papers" element={<><Navbar /><PreviousPapers /></>} />
+          <Route path="/help" element={<><Navbar /><Help /></>} />
+
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
